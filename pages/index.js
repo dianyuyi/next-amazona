337a27a0f5f47abdaff7a040460cdadc5ae9df16
Loxi <dianyuyi@gmail.com>
2021-11-17 23:03:03 +0800

    08-09_course

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout';
+import NextLink from 'next/link';
 import data from '../utils/data';
 import {
   Grid,
@@ -20,16 +21,18 @@ export default function Home() {
           {data.products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  />
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    />
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
                 <CardActions>
                   <Typography>$ {product.price}</Typography>
                   <Button size="small" color="primary">
